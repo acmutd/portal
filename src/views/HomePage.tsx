cd8c2afd5eadbf6d8c38280d86ea24923c0051fc
Harsha Srikara <harshasrikara@gmail.com>
2020-07-21 11:20:01 -0500

    minor cleanup

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import TileGrid from "../components/TileGrid/TileGrid";
-import { useAuth0 } from "@auth0/auth0-react";
 import {
   authInterface,
   loginAction,
@@ -9,31 +8,12 @@ import {
 } from "../actions/authenticate";
 import { connect } from "react-redux";
 
-interface HomePageProps {
-  isLoggedIn: boolean;
-  login(): void;
-}
+// interface HomePageProps {
+//   isLoggedIn: boolean;
+//   login(): void;
+// }
 
-const HomePage = (props: HomePageProps) => {
-  const { loginWithRedirect, isLoading } = useAuth0();
-
-  const wrapperFunction = () => {
-    // const result = callApi("/api/createDivison");
-    props.login();
-    loginWithRedirect();
-  };
-
-  // return (
-  //   <div>
-  //     <button onClick={wrapperFunction}>Click me to sign in</button>
-  //     {props.isLoggedIn && !isLoading ? (
-  //       <h1>authenticated</h1>
-  //     ) : (
-  //       <h1>Not Authenticated</h1>
-  //     )}
-  //   </div>
-  // );
-
+const HomePage = () => {
   return (
     <Fragment>
         <Navbar />
