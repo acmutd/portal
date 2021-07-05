8eee366e180305f043ef035fb39982fa6d8f6ea7
Stevenmqnguyen <34099426+Stevenmqnguyen@users.noreply.github.com>
2021-07-05 19:51:21 +0000

    Deploying to gh-pages from  @ 9b44f46aa2c665a776479e081531e8eed76618e6 🚀

---
+++ precache-manifest.4196456a3ab1ca88686337cd916fb199.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee65aa60bb83631534108b7b6f6a4b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a602df3dedb9954d35f8",
+    "url": "/static/css/2.ef440c72.chunk.css"
+  },
+  {
+    "revision": "80475971c78db78aa0f6",
+    "url": "/static/css/main.e50597a8.chunk.css"
+  },
+  {
+    "revision": "a602df3dedb9954d35f8",
+    "url": "/static/js/2.7c7ae30b.chunk.js"
+  },
+  {
+    "revision": "03bbbd373a7880b5afe17ba693f3e53f",
+    "url": "/static/js/2.7c7ae30b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80475971c78db78aa0f6",
+    "url": "/static/js/main.24983b63.chunk.js"
+  },
+  {
+    "revision": "c953884e1a18ff6b3160",
+    "url": "/static/js/runtime-main.0418dd04.js"
+  },
+  {
+    "revision": "42818f84116ffa4dcbddee4c41fb5c1c",
+    "url": "/static/media/acm-light.42818f84.png"
+  },
+  {
+    "revision": "52e5256e206312d9ffd539565c3b8924",
+    "url": "/static/media/samsungsharpsans-bold.52e5256e.otf"
+  },
+  {
+    "revision": "830454356f59ce2c9284a2d12a38fb0d",
+    "url": "/static/media/samsungsharpsans-medium.83045435.otf"
+  },
+  {
+    "revision": "9b469dd66e3bfb0c8a6a3209359b85e6",
+    "url": "/static/media/samsungsharpsans.9b469dd6.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64555c59bc2eced366f26ddc1b3aa888.js"
+  "/precache-manifest.4196456a3ab1ca88686337cd916fb199.js"
 );
 
 self.addEventListener('message', (event) => {
