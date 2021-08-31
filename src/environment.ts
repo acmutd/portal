04bebc68b24d5704f9b170c25a6b59cebf2c842b
Nicolas A. Burnett <nicolasandrewburnett@gmail.com>
2021-08-31 15:36:45 -0500

    No more firebase

--- src/environment.ts
+++ src/environment.ts
@@ -1,9 +1,2 @@
-import * as functions from "firebase-functions"
-
 let config = process.env
-const deployedToFirebase = process.env.NODE_ENV === "production"
-if (deployedToFirebase) {
-  config = functions.config().env
-}
-
 export { config as env }
