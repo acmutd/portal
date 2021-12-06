b355767efa9ab2d6259f15e5b29fb682af58036d
trexrush <edmazuera@gmail.com>
2021-12-06 12:56:43 -0600

    feat: import fonts
    currently not displaying as default font yet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 const colors = require("tailwindcss/colors");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+    },
     colors: {
       transparent: "transparent",
       current: "currentColor",
